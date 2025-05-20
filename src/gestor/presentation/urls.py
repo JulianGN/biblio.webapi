990138f21da82889049f1c638a6bcf50e06a3048
Julian Nunes <contato@juliannunes.com.br>
2025-05-19 23:24:03 -0300

    feat: dados iniciais com generos e unidades

--- src/gestor/presentation/urls.py
+++ src/gestor/presentation/urls.py
@@ -1,8 +1,13 @@
 from rest_framework.routers import DefaultRouter
-from gestor.presentation.views import LivroViewSet, UnidadeViewSet
+from django.urls import path
+from gestor.presentation.views import LivroViewSet, UnidadeViewSet, dados_iniciais
 
 router = DefaultRouter()
 router.register(r'livros', LivroViewSet, basename='livro')
 router.register(r'unidades', UnidadeViewSet, basename='unidade')
 
 urlpatterns = router.urls
+
+urlpatterns += [
+    path('dados-iniciais/', dados_iniciais, name='dados-iniciais'),
+]
--- src/gestor/presentation/views.py
+++ src/gestor/presentation/views.py
@@ -2,7 +2,10 @@ from rest_framework import viewsets
 from gestor.domain.entities.livro import Livro
 from gestor.domain.entities.unidade import Unidade
 from gestor.domain.entities.livro_unidade import LivroUnidade
+from gestor.domain.entities.genero import Genero
 from gestor.presentation.serializers import LivroSerializer, UnidadeSerializer, LivroUnidadeSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 class UnidadeViewSet(viewsets.ModelViewSet):
     queryset = Unidade.objects.all()
@@ -15,3 +18,12 @@ class LivroUnidadeViewSet(viewsets.ModelViewSet):
 class LivroViewSet(viewsets.ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
+
+@api_view(['GET'])
+def dados_iniciais(request):
+    generos = Genero.objects.all().values('id', 'nome')
+    unidades = Unidade.objects.all().values('id', 'nome', 'endereco', 'telefone', 'email', 'site')
+    return Response({
+        'generos': list(generos),
+        'unidades': list(unidades)
+    })
